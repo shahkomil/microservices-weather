dbf216baa5ef7cd5e5be27b33bd8ce410b68af6c
Komil Shah <shahkomil@gmail.com>
2022-05-09 18:28:10 -0400

    Data access to precipitation service

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,9 +1,28 @@
+using CloudWeather.Precipitation.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-app.MapGet("/observation/{zip}", (string zip) =>
+builder.Services.AddDbContext<PrecipDbContext>(opts =>
 {
-    return Results.Ok(zip);
+    opts.EnableSensitiveDataLogging();
+    opts.EnableDetailedErrors();
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+},ServiceLifetime.Transient);
+
+var app = builder.Build();
+app.MapGet("/observation/{zip}", async(string zip,[FromQuery] int? days,PrecipDbContext db) =>
+{
+    if (days == null || days < 1 || days > 30) { 
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 to 30. ");
+    }
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await  db.Precipitation
+    .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+    .ToListAsync();
+
+    return Results.Ok(results);
 });
 
 app.Run();
